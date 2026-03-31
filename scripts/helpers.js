489e3e77086780012b312595335a5d0a25aa0578
Günter Heldt <131249647+guhe78@users.noreply.github.com>
2026-03-31 17:07:46 +0200

    Feature/guhe (#64)
    * fixed bugs and failures
    
    * added users signup functionality
    
    * fixed failures signiup added functionality login signup
    
    * added login local storage signup functionality fixed unused code
    
    * added responsive contacts html
    
    * fixed functionality style responsive
    
    * added functinality mobile menu contact menu button
    
    * added user guest validation logout functionality
    
    * fixed problems after pull request

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -18,3 +18,26 @@ function getPriorityIcon(priority) {
       return "";
   }
 }
+
+function getUserData() {
+  const userData = localStorage.getItem("joinUser");
+  const data = JSON.parse(userData);
+
+  if (data.isGuest === false) {
+    return {
+      initials:
+        data.firstName[0].toUpperCase() + data.lastName[0].toUpperCase(),
+      name: data.firstName + " " + data.lastName,
+    };
+  } else {
+    return { initials: "G", name: "Guest" };
+  }
+}
+
+function checkAuth() {
+  const user = JSON.parse(localStorage.getItem("joinUser"));
+
+  if (!user) {
+    window.location.href = "../index.html";
+  }
+}
